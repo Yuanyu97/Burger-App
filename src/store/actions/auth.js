ca4083a1b5d60f3abb8ec7e39decfa8ef9b71e04
Yuanyu97 <e0322803@u.nus.edu>
2020-06-14 21:22:20 +0800

    Add authentication

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,16 +24,19 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     }
 }
 
-export const checkAuthTimeout = (expirationTime) => {
+export const checkAuthTimeout = (expirationDate) => {
     return dispatch => {
         setTimeout(() => {
             dispatch(logout());
-        }, expirationTime * 1000)
+        }, expirationDate * 1000)
     }
 }
 
@@ -52,6 +55,10 @@ export const auth = (email, password, isSignUp) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -67,3 +74,21 @@ export const setAuthRedirectPath = (path) => {
         path: path
     }
 }
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date()) {
+                dispatch((logout()));
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime()) / 1000));
+            }
+        }
+    };
+};
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -3,5 +3,6 @@ export { purchaseBurger, purchaseInit, fetchOrders } from './order';
 export {
     auth,
     logout,
-    setAuthRedirectPath
+    setAuthRedirectPath,
+    authCheckState
 } from './auth'
